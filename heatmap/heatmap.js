a90bd3be639290adb2087503f846893fdb48ad25
Wesley Reardan <reardan@wisc.edu>
2014-07-17 18:40:09 -0500

    Added the ability to load matrix files in to the heatmap

--- heatmap/heatmap.js
+++ heatmap/heatmap.js
@@ -6,6 +6,7 @@ var colors_rgb = [
 	[64, 0, 0],
 	[200, 0, 0]
 ]
+//this function interpolates a value from a color range
 function convert_to_rgb(minval, maxval, val, colors) {
 	var result = {'r': 0, 'g': 0, 'b': 0}
 	
@@ -29,6 +30,8 @@ function convert_to_rgb(minval, maxval, val, colors) {
 	return result
 }
 
+//this functions converts an color (with .r .g .b values)
+//to a context.fillStyle for canvas drawing
 function convert_rgb_to_fillstyle(color) {
 	var fill = 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')'
 	return fill
@@ -64,38 +67,77 @@ function draw_matrix(matrix) {
 	}
 }
 
-//load a matrix from a datafile
-function load_matrix(filename, callback) {
+//load a matrix from a datafile then do callback(matrix)
+function load_matrix(data) {
+	//future parameters
 	var delimiter = '\t'
 	var datatype = Number
+
+	//the resulting parsed matrix
+	var matrix = []
+	//split file into lines
+	var lines = data.split('\n')
+	//split lines into columns
+	for(var i = 0; i < lines.length; i++) {
+		var line = lines[i]
+		var columns = line.split(delimiter)
+		//set matrix row
+		var row = []
+		for(var j = 0; j < columns.length; j++) {
+			var value = datatype(columns[j])
+			row.push(value)
+		}
+		matrix.push(row)
+	}
+
+	return matrix
+}
+
+//load a matrix from a filename, then call function
+function load_matrix_file(filename, callback) {
 
 	var request = new XMLHttpRequest()
 
 	//setup request handler
 	request.onload = function() {
-		var matrix = []
-		//split file into lines
 		var data = this.responseText
-		var lines = data.split('\n')
-		//split lines into columns
-		for(var i = 0; i < lines.length; i++) {
-			var line = lines[i]
-			var columns = line.split(delimiter)
-			//set matrix row
-			var row = []
-			for(var j = 0; j < columns.length; j++) {
-				var value = datatype(columns[j])
-				row.push(value)
-			}
-			matrix.push(row)
-		}
+		var matrix = load_matrix(data, callback)
 		callback(matrix)
 	}
 	request.open('get', filename, true)
 	request.send()
+
 }
 
+//Called when file(s) are uploaded
+function handle_file_selected(event) {
+	var files = event.target.files
+	var encoding = 'utf8'
+
+	for(var i = 0; i < files.length; i++) {
+		var file = files[i]
+		//console.log('file.name: %s, file.size: %d', file.name, file.size)
+
+		//http://blog.teamtreehouse.com/reading-files-using-the-html5-filereader-api
+		var reader = new FileReader()
+		reader.onload = function(e) {
+			var text = reader.result
+			//console.log('file.name: %s, file data size: %d', file.name, text.length)
+			//load matrix from text
+			var matrix = load_matrix(text)
+			draw_matrix(matrix)
+		}
+		reader.readAsText(file, encoding)
+	}
+}
+
+//called after document has been loaded
 function main() {
 	var filename = './data/marked_MiniBatchKMeans.txt'
-	load_matrix(filename, draw_matrix)
+	load_matrix_file(filename, draw_matrix)
+
+	//setup event handler for file upload
+	//http://www.html5rocks.com/en/tutorials/file/dndfiles/
+	document.getElementById('files').addEventListener('change', handle_file_selected, false)
+
 }
